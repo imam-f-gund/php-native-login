54f8e18bcd4982a9dc8d4a5ba4d2153d33f16ced
imam-f-gund <imameks03@gmail.com>
2022-12-07 15:27:44 +0700

    update

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -9,9 +9,10 @@ include 'header.php';
   <div class="card-header text-center">Information</div>
   <div class="card-body">
     <h5 class="card-title text-center">Welcome <?php echo $_SESSION['user']; ?></h5>
-    <p class="card-text text-center">Content.....</p>
+    <p class="card-text text-center">Content...</p>
   </div>
 </div>
+
 <script src="../asset/js/alert.js"></script>
 <script>
   Alertlogin();
